da407b3d43598eebc64846c0adf8f522e9cac359
Johnny Kim <jxkim89@gmail.com>
2018-04-30 22:21:41 -0400

    latest

--- example.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-hasher = hashlib.new('md5')
-with open('hi.txt', 'rb') as afile:
-        buf = afile.read()
-        # print("buf: "+buf)
-        hasher.update(buf)
-        hash = hasher.hexdigest()
-        print(hash)
-        # print("buffer" + buf)
